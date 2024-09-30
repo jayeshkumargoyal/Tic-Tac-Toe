e4fbb38a8e52c0029af0efb5f0316a97b593204e
jayeshkumargoyal <kumarjayesh012@gmail.com>
2024-09-30 01:13:18 -0600

    Draw condition Implemented

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true // playerX, playerO
+let turnO = true; // playerX, playerO
 const winPatterns = [
     [0,1,2],
     [0,3,6],
@@ -22,13 +22,19 @@ const resetGame = () => {
     msgContainer.classList.add("hide");
 }
 
+const isDraw = () => {
+    return [...boxes].every(box => box.innerText !== "");
+}
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnO) {
             box.innerText = "O";
+            box.classList.add("O");
             turnO = false;
         } else {
             box.innerText = "X";
+            box.classList.add("X");
             turnO = true;
         }
         box.disabled = true;
@@ -47,6 +53,7 @@ const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
+        box.classList.remove("X", "O");
     }
 }
 
@@ -56,20 +63,33 @@ const showWinner = (winner) => {
     disableBoxes();
 }
 
+const showDraw = () => {
+    msg.innerText = `It's a Draw!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
 const checkWinner = () => {
+    let winnerDeclared = false;
+
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
-    
-    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "" ) {
-        if (pos1Val == pos2Val && pos2Val == pos3Val) {
-            showWinner(pos1Val);
+
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val == pos2Val && pos2Val == pos3Val) {
+                showWinner(pos1Val);
+                winnerDeclared = true;
+                break;
+            }
         }
     }
-    }
 
+    if (!winnerDeclared && isDraw()) {
+        showDraw();
+    }
 }
 
-newGameBtn.addEventListener("click",resetGame);
-resetBtn.addEventListener("click",resetGame);
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
